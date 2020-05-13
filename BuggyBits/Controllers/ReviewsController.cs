30e7048b51c306bc09bd65c226c6b86783df93d9
ustabar <ustabar@outlook.com>
2020-05-13 22:38:46 +0300

    test

---
+++ BuggyBits/Controllers/ReviewsController.cs
@@ -0,0 +1,50 @@
+﻿using BuggyBits.Models;
+using BuggyBits.ViewModels;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+
+namespace BuggyBits.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ThreadPool.GetMinThreads(out int minWorker, out int minIo);
+            ThreadPool.GetMaxThreads(out int maxWorker, out int maxIo);
+            
+            ViewData["minWorker"] = minWorker;
+            ViewData["maxWorker"] = maxWorker;
+            ViewData["minIo"] = minIo;
+            ViewData["maxIo"] = maxIo;
+
+            ViewData["serverName"] = System.Net.Dns.GetHostName();
+
+            var isRefresh = Request.Query["refresh"] == "1";
+
+            if (isRefresh)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+            }
+
+            var model = new ReviewsViewModel();
+
+            Review rev = new Review();
+            rev.GenerateReview1();
+            model.Review1 = new HtmlString("<I>" + rev.quote + "</I>");
+            model.Source1 = new HtmlString("&nbsp;&nbsp;-" + rev.source);
+            rev.ClearReview();
+
+            Review rev2 = new Review();
+            rev2.GenerateReview2();
+            model.Review2 = new HtmlString("<I>" + rev2.quote + "</I>");
+            model.Source2 = new HtmlString("&nbsp;&nbsp;-" + rev2.source);
+            rev2.ClearReview();
+
+            return View(model);
+        }
+    }
+}
---
+++ BuggyBits/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using BuggyBits.Models;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BuggyBits
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddMemoryCache();
+
+            services.AddSingleton(_ => new DataLayer());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
